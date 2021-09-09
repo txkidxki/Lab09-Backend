d33da7056b28cfc4faa315e431d2f0881c388628
txkidxki <rattanakorn_re@cmu.ac.th>
2021-09-09 19:09:13 +0700

    2.1

--- src/main/java/se331/lab/rest/entity/Event.java
+++ src/main/java/se331/lab/rest/entity/Event.java
@@ -2,10 +2,7 @@ package se331.lab.rest.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.List;
 
 @Data
@@ -25,6 +22,8 @@ public class Event {
     String date;
     String time;
     Boolean petAllowed;
+    @ManyToOne
     Organizer organizer;
+    @ManyToMany(mappedBy = "eventHistory")
     List<Participant> Participants;
 }
